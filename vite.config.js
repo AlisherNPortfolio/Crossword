556b3e01fa990d4ca07512bb7683d665a0f48902
Alisher <myemail9445@inbox.ru>
2025-03-11 13:51:43 +0500

    vite cors problem solved

--- vite.config.js
+++ vite.config.js
@@ -5,18 +5,15 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/css/dashboard.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                }
-            }
-        }),
+        vue({}),
     ],
+    server: {
+        // origin: 'http://127.0.0.1:5173',
+        cors: true
+    },
     resolve: {
         alias: {
             '@': '/resources/js',
