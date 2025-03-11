bb529980ecfa0ab4d3fac63897d38188bfd2e631
Alisher <myemail9445@inbox.ru>
2025-03-11 09:40:59 +0500

    user profile, role and permission models

---
+++ app/Models/Role.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description'
+    ];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class);
+    }
+
+    public function hasPermission(string $permissionSlug)
+    {
+        return $this->permissions()->where('slug', $permissionSlug)->exists();
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,12 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -23,6 +22,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'profile_photo',
+        'active',
+        'last_login_at'
     ];
 
     /**
@@ -45,11 +47,69 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'active' => 'boolean',
+            'last_login_at' => 'datetime'
         ];
+    }
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    public function profile()
+    {
+        return $this->hasOne(UserProfile::class);
     }
 
     public function crosswords(): HasMany
     {
         return $this->hasMany(Crossword::class, 'created_by');
+    }
+
+    public function solutions()
+    {
+        return $this->hasMany(UserSolution::class);
+    }
+
+    // public function competitionResults()
+    // {
+    //     return $this->hasMany(CompititionResult::class);
+    // }
+
+    public function hasRole(string $roleSlug)
+    {
+        return $this->roles()->where('slug', $roleSlug)->exists();
+    }
+
+    public function hasAnyRole(array $roleSlugs)
+    {
+        return $this->roles()->whereIn('slug', $roleSlugs)->exists();
+    }
+
+    public function hasPermission(string $permissionSlug)
+    {
+        foreach ($this->roles as $role) {
+            if($role->permissions()->where('slug', $permissionSlug)->exists()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function isAdmin()
+    {
+        return $this->hasRole('administrator');
+    }
+
+    public function isCreator()
+    {
+        return $this->hasRole('creator');
+    }
+
+    public function isSolver()
+    {
+        return $this->hasRole('solver');
     }
 }
